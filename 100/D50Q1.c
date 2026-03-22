7fd8aa6134818481ba27658dbbd1fb8d02ed96ba
suvanshbansal <suvanshbansal2006@gmail.com>
2026-03-22 22:13:59 +0530

    Day50

---
+++ 100/D50Q1.c
@@ -0,0 +1,50 @@
+/*
+Problem: BST Search
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    // Base case: root is NULL or value found
+    if (root == NULL || root->val == val) {
+        return root;
+    }
+
+    // If value is smaller, search in left subtree
+    if (val < root->val) {
+        return searchBST(root->left, val);
+    }
+
+    // If value is greater, search in right subtree
+    return searchBST(root->right, val);
+}
+
+//iterative approach code
+/*
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    while (root != NULL) {
+        if (root->val == val) {
+            return root;
+        } else if (val < root->val) {
+            root = root->left;
+        } else {
+            root = root->right;
+        }
+    }
+    return NULL;
+}
+    */
